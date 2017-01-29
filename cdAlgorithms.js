43272f14f2786800c7f090f25b5065bbde01b3a6
Scott Langdon <scottlangdon@Scotts-MacBook-Air.local>
2017-01-29 12:09:44 -0800

    Completed cdalgos

--- cdAlgorithms.js
+++ cdAlgorithms.js
@@ -1,4 +1,104 @@
+// Negative 
+function numToStr(arr) {
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]<0){
+            arr[i] = 'Negative';
+        }
+    }
+    return arr;
+}
 
+
+// Swap Values
+function swap(arr) {
+    var temp = arr[0];
+    arr[0] = arr[arr.length - 1];
+    arr[arr.length - 1] = temp;
+    return arr;
+}
+
+// Max Min Avg
+function maxMinAvg(arr) {
+    var max = arr[0];
+    var min = arr[0];
+    var sum = arr[0];
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+        if(arr[i]<min){
+            min = arr[i];
+        }
+        sum = sum + arr[i];
+    }
+    var avg = sum/arr.length;
+    var newArray = [max,min,avg];
+    return newArray;
+}
+
+// Swap negatives with: 0
+function noNeg(arr) {
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]<0){
+            arr[i] = 0;
+        }
+    }
+    return arr;
+}
+
+// Squares each item in the array
+function squareVal(arr) {
+    var newArray = [];
+    for(var i=0; i<arr.length; i++){
+        newArray.push(arr[i]*arr[i]);
+    }
+    return newArray;
+}
+
+// Greater that Y
+function greaterY(arr, Y) {
+    var count = 0;
+    for(var i=0; i<arr.length; i++){
+      if(arr[i]>Y){
+        count++
+      }
+    }
+    return count;
+}
+
+// Array Odd
+function oddNumbers(arr) {
+    var newArray = [];
+    for(var i=0; i<arr.length; i++){
+      if(arr[i] % 2 === 1) {
+        newArray.push(arr[i]);
+      }
+    }
+    return newArray;
+}
+
+
+// Find Average
+function findAvg(arr) {
+    var sum = 0;
+    var avg = 0;
+    for(var i=0; i<arr.length; i++){
+        sum += arr[i];
+    }
+    avg = sum/arr.length;
+    return avg;
+}
+
+// Find Max
+function findMax(arr){
+	var max = arr[0];
+	for(var i=0; i<arr.length; i++){
+		if(arr[i] > max){
+			max = arr[i];
+		}
+	}
+	return max;
+}
 
 // Iterate an array
 function iterArr(arr){
